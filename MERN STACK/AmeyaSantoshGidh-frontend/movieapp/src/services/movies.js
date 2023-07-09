b2231aee910712439093b9147bd41dd07c5cca0b
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-07-09 11:49:55 -0400

    Add files via upload

---
+++ MERN STACK/AmeyaSantoshGidh-frontend/movieapp/src/services/movies.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+class MoviesDataService {
+
+    getAll(page = 0){
+        return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies?page=${page}`);
+    }
+
+    find(query, by="title", page=0){
+        return axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/v1/movies?${by}=${query}&page=${page}`
+        );
+    }
+
+    get(id){
+        return axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/id/${id}`
+        );
+    }
+
+    getRatings(){
+        return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/ratings`);
+    }
+
+    createReview(data){
+        return axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/review`,data);
+        
+    }
+// change
+    editReview(data){
+        return axios.put(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/review`, data)
+        
+    }
+    
+    DeleteReview(data){
+        return axios.delete(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/review`,{data})
+    }
+
+}
+export default new MoviesDataService();
