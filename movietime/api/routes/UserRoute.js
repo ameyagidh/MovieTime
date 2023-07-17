a1e8bcd8a9bee5f0b61de6a9c114ec92d3c6ebbb
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-07-16 23:04:09 -0400

    Add files via upload

--- movietime/api/routes/UserRoute.js
+++ movietime/api/routes/UserRoute.js
@@ -33,12 +33,79 @@ Router.put("/:id",verify,async(req,res)=>{
 
 // DELETE
 
+Router.delete("/:id",verify,async(req,res)=>{
+
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try{
+            await User.findByIdAndDelete(req.params.id)
+            res.status(201).json("User successfully Deleted");
+    
+        }catch(err){
+                res.status(403).json(err);
+        }
+    }else{
+        res.status(500).json("You are not authorized to delete this user");
+    }
 
 
-// GeET
+});
 
+
+// GET
+Router.get("/find/:id",async(req,res)=>{
+    const requiredUser = await User.findById(req.user.id);
+    const [password,...info] = requiredUser._doc;
+    if(requiredUser){
+        res.status(200).json({info});
+    }else{
+        res.status(401).json("User not found");
+    }
+
+})
 // GET ALL
+Router.get("/",verify,async(req,res)=>{
+    const query_ = req.query.new;
+    if(req.user.isAdmin){
+        try{
+           
+            const users_all =  query_ ? await User.find().sort({_id:-1}).limit(10): await User.find();
+            res.status(201).json(users_all);
+    
+        }catch(err){
+                res.status(403).json(err);
+        }
+    }else{
+        res.status(500).json("You are not authorized to view all the user");
+    }
 
-// USER STATS
+
+});
+
+
+//GET USER STATS
+Router.get("/stats", async (req, res) => {
+    const today = new Date();
+    const latYear = today.setFullYear(today.setFullYear() - 1);
+  
+    try {
+      const data = await User.aggregate([
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+          },
+        },
+      ]);
+      res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = Router;
