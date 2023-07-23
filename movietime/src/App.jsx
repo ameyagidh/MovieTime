2f45698802461653cafd90d2bb5e1c6625bbb08f
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-07-22 21:19:17 -0400

    React Router Dom

--- movietime/src/App.jsx
+++ movietime/src/App.jsx
@@ -3,10 +3,28 @@ import Home from "./pages/home/Home";
 import RegisterComponent from "./pages/register/RegisterComponent";
 import MyLoginComponent from "./pages/login/MyLoginComponent";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 const App = () => {
-  return <Home/>;
+  // return <Home/>; 
   // return <RegisterComponent/>
   // return <MyLoginComponent/>
+
+
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="register" element={<RegisterComponent />} />
+      <Route path="login" element={<MyLoginComponent />} />
+    </Routes>
+  </BrowserRouter>
+  );
+
 };
 
 export default App;
